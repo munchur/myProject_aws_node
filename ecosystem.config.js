0d1b88a70a320baaeee008c2ec39432e18ee0f54
Cher Moua <mouacher@pdx.edu>
2019-03-06 15:18:45 -0800

    setting up pm2 local

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -6,11 +6,11 @@ module.exports = {
     deploy: {
         production: {
             user: 'ubuntu',
-            host: 'ec2-18-191-173-13.us-east-2.compute.amazonaws.com',
+            host: 'ec2-18-220-176-227.us-east-2.compute.amazonaws.com',
             key: '~/.ssh/myProject.pem.txt',
             ref: 'origin/master',
             repo: 'git@github.com:munchur/myProject_aws_node.git',
-            path: '/home/ubuntu/myProject2',
+            path: '/home/ubuntu/myProject_aws_node',
             'post-deploy': 'npm install && pm2 startOrRestart ecosystem.config.js'
         }
     }
