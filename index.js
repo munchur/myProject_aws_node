3aeebb99566abbbbee85b8ec89a55ee9b08f3222
Cher Moua <mouacher@pdx.edu>
2019-03-07 17:17:48 -0800

    finished product

--- index.js
+++ index.js
@@ -1,7 +1,8 @@
 const express = require('express')
 const app = express()
-app.get('/', (req, res) => {
-    res.send('HEY! there')
-})
+//app.get('/', (req, res) => {
+//    res.send('HEY! there on local')
+//})
 
-app.listen(3000, () => console.log('Server runnong on port 3000'))
+app.use(express.static('public'))
+app.listen(3000, () => console.log('Server running on port 3000'))
